8c9fd721ddd1bdeb1c1c33ac22787a7937080d35
rodwanz <wanzervai@gmail.com>
2023-11-11 21:33:57 -0300

    Nada melhor que acertar

--- src/main/java/com/wanzeler/controleacesso/api/exceptionhandler/ApiControleExcecao.java
+++ src/main/java/com/wanzeler/controleacesso/api/exceptionhandler/ApiControleExcecao.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.wanzeler.controleacesso.domain.exception.AutorizacaoPrestacaoDeServicoException;
+import com.wanzeler.controleacesso.domain.exception.AutorizacaoUsoDeVagaException;
 import com.wanzeler.controleacesso.domain.exception.PlanilhaControleException;
 
 @ControllerAdvice
@@ -15,6 +17,30 @@ public class ApiControleExcecao{
 	@ExceptionHandler(PlanilhaControleException.class)
 	public ResponseEntity<?> tratandoControleExcption(
 			PlanilhaControleException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(AutorizacaoUsoDeVagaException.class)
+	public ResponseEntity<?> tratandoUsoDeVagaExcption(
+			AutorizacaoUsoDeVagaException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(AutorizacaoPrestacaoDeServicoException.class)
+	public ResponseEntity<?> tratandoUsoDeVagaExcption(
+			AutorizacaoPrestacaoDeServicoException e) {
 		Problema problema = Problema.builder()
 				.dataHora(LocalDateTime.now())
 				.mensagem(e.getMessage())
